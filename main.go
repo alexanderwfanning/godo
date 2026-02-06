ff4ce4b591fd94763290696431a2b6c40271c9d4
alexanderwfanning <alexanderwfanning@gmail.com>
2026-02-06 17:36:25 +0000

    printing multiple

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ type Tile struct {
 }
 
 func printTile(t Tile) {
-	fmt.Println(t.title)
-	for _, v := range t.todo {
-		fmt.Println("-", v)
+	fmt.Println("\n", t.title)
+	for i, v := range t.todo {
+		fmt.Printf("%d. %s\n", i+1, v)
 	}
 }
 
 func main() {
 	myTile := Tile{title: "First Things First", todo: []string{"test", "another task", "yet another task", "and another and another"}}
-	printTile(myTile)
+	anotherTile := Tile{title: "Secondary Things Second", todo: []string{"Make this work", "And make it good"}}
+	tertiaryTile := Tile{title: "Tertiary Things Third", todo: []string{"Bubble Tea looks cool!", "Maybe I should use that?"}}
+	allMyTiles := []Tile{myTile, anotherTile, tertiaryTile}
+	for _, tile := range allMyTiles {
+		printTile(tile)
+	}
 }
